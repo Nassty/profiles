7a7a68a824c9087aae2e1f5bbaa2d101315450c2
Mariano Garcia Berrotaran <mgarcia@machinalis.com>
2017-06-02 14:09:14 -0300

    0.3

--- src/store.js
+++ src/store.js
@@ -1,6 +1,8 @@
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
 import reducers from './reducers'
 
-const store = createStore(reducers, {});
+import logger from 'redux-logger';
+
+const store = createStore(reducers, applyMiddleware(logger));
 
 export default store;

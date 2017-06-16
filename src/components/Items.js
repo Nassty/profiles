d5029cde0f6ccdd9098c647fb203f015bc69fbe2
Mariano Garcia Berrotaran <mgarcia@machinalis.com>
2017-06-16 12:49:36 -0300

    0.9

--- src/components/Items.js
+++ src/components/Items.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import {connect} from 'react-redux';
+import {createSelector} from 'reselect';
 
-const Items = ({items}) => (
+const Items = ({item}) => (
   <pre>
-    {JSON.stringify(items, null, 4)}
+    {JSON.stringify(item, null, 4)}
   </pre>
 );
 
+const getSelected = state => state.selected;
+const getItems = state => state.items;
+const itemsSelector = createSelector(
+  [getSelected, getItems],
+  (selected, items) => items[selected]
+);
+
 const mapStateToProps = state => ({
-  items: state.items
+  item: itemsSelector(state)
 });
 
 export default connect(mapStateToProps)(Items);
